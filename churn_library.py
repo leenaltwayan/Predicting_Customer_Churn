1e7c9c2c75de738fcdbd5bf2260c5de5a813a0b9
leen <leenaltwayan@gmail.com>
2021-12-19 11:26:41 +0300

    improved pylint results

--- churn_library.py
+++ churn_library.py
@@ -3,23 +3,30 @@
 Author: Leen
 Version: 0.1
 Date: 12/12/2021
-This module provides a set of functions that will predict customer churn,
-guaranteeing readability and modularization
+This module provides a set of functions that will predict customer churn, including:
+- Data Import
+- Data Preparation
+- Visualization
+- Model Training
+- Model Save and Export
+- Evaluation metrics and plot
 '''
 
 # import libraries
+import os
+import warnings
 from sklearn.metrics import classification_report, plot_roc_curve
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-import os
-os.environ['QT_QPA_PLATFORM']='offscreen'
 import scikitplot as skplt
 import pandas as pd
 import numpy as np
 import joblib
 import matplotlib.pyplot as plt
 import seaborn as sns
+warnings.filterwarnings("ignore")
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 sns.set()
 
 
@@ -82,6 +89,7 @@ def perform_eda(df):
     except BaseException as msg:
         print("Exception in perform_eda: ", msg)
 
+
 def encoder_helper(df, category_lst, response):
     '''
     helper function to turn each categorical column into a new column with
@@ -125,8 +133,8 @@ def perform_feature_engineering(df, response):
     '''
     input:
               df: pandas dataframe
-              response: string of response name [optional argument that could be used for naming variables or index y column]
-
+              response: string of response name [optional argument that could be used for naming
+              variables or index y column]
     output:
               X_train: X training data
               X_test: X testing data
@@ -184,25 +192,37 @@ def classification_report_image(y_train,
         plt.rc('figure', figsize=(5, 5))
         plt.text(0.01, 1.25, str('Random Forest Train'), {
                  'fontsize': 10}, fontproperties='monospace')
-        plt.text(0.01, 0.05, str(classification_report(y_test, y_test_preds_rf)), {
-                 'fontsize': 10}, fontproperties='monospace')  # approach improved by OP -> monospace!
-        plt.text(0.01, 0.6, str('Random Forest Test'), {
-                 'fontsize': 10}, fontproperties='monospace')
-        plt.text(0.01, 0.7, str(classification_report(y_train, y_train_preds_rf)), {
-                 'fontsize': 10}, fontproperties='monospace')  # approach improved by OP -> monospace!
+        plt.text(
+            0.01, 0.05, str(
+                classification_report(
+                    y_test, y_test_preds_rf)), {
+                'fontsize': 10}, fontproperties='monospace')
+        plt.text(0.01, 0.6, str('Random Forest Test'),
+                 {'fontsize': 10}, fontproperties='monospace')
+        plt.text(
+            0.01, 0.7, str(
+                classification_report(
+                    y_train, y_train_preds_rf)), {
+                'fontsize': 10}, fontproperties='monospace')
         plt.axis('off')
         plt.savefig('images/results/random_forest_report.png')
 
         plt.rc('figure', figsize=(5, 5))
         plt.text(0.01, 1.25, str('Logistic Regression Train'),
                  {'fontsize': 10}, fontproperties='monospace')
-        plt.text(0.01, 0.05, str(classification_report(y_train, y_train_preds_lr)), {
-                 'fontsize': 10}, fontproperties='monospace')  # approach improved by OP -> monospace!
-        plt.text(0.01, 0.6, str('Logistic Regression Test'), {
-                 'fontsize': 10}, fontproperties='monospace')
+        plt.text(
+            0.01, 0.05, str(
+                classification_report(
+                    y_train, y_train_preds_lr)), {
+                'fontsize': 10}, fontproperties='monospace')
+        plt.text(0.01, 0.6, str('Logistic Regression Test'),
+                 {'fontsize': 10}, fontproperties='monospace')
 
-        plt.text(0.01, 0.7, str(classification_report(y_test, y_test_preds_lr)), {
-                 'fontsize': 10}, fontproperties='monospace')  # approach improved by OP -> monospace!
+        plt.text(
+            0.01, 0.7, str(
+                classification_report(
+                    y_test, y_test_preds_lr)), {
+                'fontsize': 10}, fontproperties='monospace')
         plt.axis('off')
         plt.savefig('images/results/logistic_regression_report.png')
 
@@ -297,7 +317,7 @@ def train_models(X_train, X_test, y_train, y_test):
         print(y_test)
         plot_roc_curve(lrc, X_test, y_test)
         plt.savefig('images/results/lr_roc_auc.png')
-        feature_importance_plot(cv_rfc, X_train,'images/results/')
+        feature_importance_plot(cv_rfc, X_train, 'images/results/')
 
         # plots
         plt.figure(figsize=(15, 8))
@@ -316,7 +336,9 @@ def train_models(X_train, X_test, y_train, y_test):
         joblib.dump(lrc, './models/logistic_model.pkl')
 
     except AssertionError as msg:
-        print('in train_models: Incorrect parameters; make sure X values are dataframes and Y values are a list')
+        print(
+            'in train_models: Incorrect parameters; make sure X values are dataframes \
+        and Y values are a list')
         print(msg)
     except BaseException as err:
         print('Exception in Training function: ', err)
@@ -324,40 +346,58 @@ def train_models(X_train, X_test, y_train, y_test):
 
 
 if __name__ == "__main__":
-    
+
     df = pd.read_csv("./data/bank_data.csv")
-    df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+    df['Churn'] = df['Attrition_Flag'].apply(
+        lambda val: 0 if val == "Existing Customer" else 1)
     pth = "./data/bank_data.csv"
-    
+
     print(import_data(pth))
     perform_eda(df)
-    
-    category_lst = ['Gender','Education_Level','Marital_Status',
-            'Income_Category','Card_Category']
-    response = ['Gender_Churn','Education_Level_Churn',
-            'Marital_Status_Churn','Income_Category_Churn', 'Card_Category_Churn']
+
+    category_lst = ['Gender', 'Education_Level', 'Marital_Status',
+                    'Income_Category', 'Card_Category']
+    response = [
+        'Gender_Churn',
+        'Education_Level_Churn',
+        'Marital_Status_Churn',
+        'Income_Category_Churn',
+        'Card_Category_Churn']
     encoded_df = encoder_helper(df, category_lst, response)
 
     print(encoded_df)
-    
+
     y = df['Churn']
     X = pd.DataFrame()
-    keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
-             'Total_Relationship_Count', 'Months_Inactive_12_mon',
-             'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
-             'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
-             'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
-             'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn', 
-             'Income_Category_Churn', 'Card_Category_Churn']
-    
+    keep_cols = [
+        'Customer_Age',
+        'Dependent_count',
+        'Months_on_book',
+        'Total_Relationship_Count',
+        'Months_Inactive_12_mon',
+        'Contacts_Count_12_mon',
+        'Credit_Limit',
+        'Total_Revolving_Bal',
+        'Avg_Open_To_Buy',
+        'Total_Amt_Chng_Q4_Q1',
+        'Total_Trans_Amt',
+        'Total_Trans_Ct',
+        'Total_Ct_Chng_Q4_Q1',
+        'Avg_Utilization_Ratio',
+        'Gender_Churn',
+        'Education_Level_Churn',
+        'Marital_Status_Churn',
+        'Income_Category_Churn',
+        'Card_Category_Churn']
+
     X[keep_cols] = encoded_df[keep_cols]
     print('y shape is: ', y.shape)
     print(y.dtype)
     print('X shape is: ', X.shape)
     perform_feature_engineering(encoded_df, 'Churn')
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=42) 
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42)
     print(y_train.dtype)
     print(y_test.dtype)
     train_models(X_train, X_test, y_train, y_test)
-    
--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,9 +1,22 @@
+'''
+Author: Leen
+Version: 0.1
+Date: 12/12/2021
+This module tests a set of functions used in churn_library, including:
+- test_import
+- test_eda
+- test_encoder_helper
+- test_perform_feature_engineering
+- test_train_models
+'''
 import os
-os.environ['QT_QPA_PLATFORM']='offscreen'
 import logging
-import churn_library
-import pandas as pd
+import warnings
 from sklearn.model_selection import train_test_split
+import pandas as pd
+import churn_library
+warnings.filterwarnings("ignore")
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 
 
 logging.basicConfig(
@@ -13,21 +26,21 @@ logging.basicConfig(
     format='%(name)s - %(levelname)s - %(message)s')
 
 
-def test_import(pth):
+def test_import(file_path):
     '''
     test data import - this example is completed for you to assist with the other test functions
         '''
     try:
-        df = churn_library.import_data(pth)
+        df = churn_library.import_data(file_path)
         print(df.head())
         logging.info("Testing import_data: SUCCESS")
-    except FileNotFoundError as err:
+    except FileNotFoundError:
         logging.error("Testing import_eda: The file wasn't found")
-        
+
     try:
         assert df.shape[0] > 0
         assert df.shape[1] > 0
-    except AssertionError as err:
+    except AssertionError:
         logging.error(
             "Testing import_data: The file doesn't appear to have rows and columns")
 
@@ -42,13 +55,10 @@ def test_eda(df):
         assert df.shape[1] > 0
         churn_library.perform_eda(df)
         logging.info("Testing perform_eda: SUCCESS")
-    except AssertionError as msg:
+    except AssertionError:
         logging.error("Testing perform_eda: Empty or Non-existent dataframe")
-#         raise err
-    except BaseException as msg:
-        print("error message in test_eda: ",msg)
+    except BaseException:
         logging.error("Testing perform_eda: Could not run perform_eda")
-#         raise err
 
 
 def test_encoder_helper(df, category_lst, response):
@@ -63,12 +73,12 @@ def test_encoder_helper(df, category_lst, response):
         assert encoded_df.shape[0] > 0
         assert encoded_df.shape[1] > 0
         logging.info("Testing encoder_helper: SUCCESS")
-    except AssertionError as err:
+    except AssertionError:
         logging.error(
             "Testing encoder_helper: The given Dataframe doesn't appear to have rows and columns")
-    except BaseException as err:
+    except BaseException:
         logging.error(
-            "Testing encoder_helper: BaseException: ", err)
+            "Testing encoder_helper: Basic exception")
 
 
 def test_perform_feature_engineering(df, response):
@@ -84,7 +94,8 @@ def test_perform_feature_engineering(df, response):
             "Testing perform_feature_engineering: the given parameters are incorrect types")
         print("in test_perform_feature_engineering: incorrect parameter types")
     try:
-        X_train, X_test, y_train, y_test = churn_library.perform_feature_engineering(df, response)
+        X_train, X_test, y_train, y_test = churn_library.perform_feature_engineering(
+            df, response)
         assert X_train.shape[0] > 0
         assert X_train.shape[1] > 0
         assert X_test.shape[0] > 0
@@ -93,11 +104,12 @@ def test_perform_feature_engineering(df, response):
         assert y_test.shape[0] > 0
         logging.info("Testing perform_feature_engineering: SUCCESS")
     except TypeError as err:
-        logging.error('Testing perform_feature_engineering: TypeError: ', err)
-        print("error in testing perform feature engineering: ",err)
-    except AssertionError as err:
         logging.error(
-            "Testing perform_feature_engineering: The output variables dont appear to have rows or columns")
+            'Testing perform_feature_engineering: Type error occurred. Please check parameters.')
+    except AssertionError:
+        logging.error(
+            "Testing perform_feature_engineering: The output variables \
+            dont appear to have rows or columns")
 
 
 def test_train_models(X_train, X_test, y_train, y_test):
@@ -113,47 +125,51 @@ def test_train_models(X_train, X_test, y_train, y_test):
         assert y_test.shape[0] > 0
         churn_library.train_models(X_train, X_test, y_train, y_test)
         logging.info("Testing train_models: SUCCESS")
-    except AssertionError as err:
+    except AssertionError:
         logging.error(
             "Testing train_models: The given parameters dont appear to have rows or columns")
-    except BaseException as err:
+    except BaseException:
         logging.error("Testing train_models: Basic Exception")
-        print("error msg in test train_models: ", err)
-#         raise err
 
 
 if __name__ == "__main__":
-    
+
     df = pd.read_csv("./data/bank_data.csv")
-    df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+    df['Churn'] = df['Attrition_Flag'].apply(
+        lambda val: 0 if val == "Existing Customer" else 1)
     pth = "./data/bank_data.csv"
-    
+
     test_import(pth)
     test_import('not_real_path')
     test_eda(df)
     test_eda(pd.DataFrame())
-    
-    category_lst = ['Gender','Education_Level','Marital_Status',
-            'Income_Category','Card_Category']
-    response = ['Gender_Churn','Education_Level_Churn',
-            'Marital_Status_Churn','Income_Category_Churn', 'Card_Category_Churn']
-    test_encoder_helper(df, category_lst, response)
+
+    cat_list = ['Gender', 'Education_Level', 'Marital_Status',
+                'Income_Category', 'Card_Category']
+    resp_list = [
+        'Gender_Churn',
+        'Education_Level_Churn',
+        'Marital_Status_Churn',
+        'Income_Category_Churn',
+        'Card_Category_Churn']
+    test_encoder_helper(df, cat_list, resp_list)
     test_encoder_helper(df, 'um', 'yes')
-    encoded_df = churn_library.encoder_helper(df, category_lst, response)
-    
+    encoded_df = churn_library.encoder_helper(df, cat_list, resp_list)
+
     y = df['Churn']
     X = pd.DataFrame()
-    keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
-             'Total_Relationship_Count', 'Months_Inactive_12_mon',
-             'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
-             'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
-             'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
-             'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn', 
-             'Income_Category_Churn', 'Card_Category_Churn']
-    
-    X[keep_cols] = encoded_df[keep_cols]
+    cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
+            'Total_Relationship_Count', 'Months_Inactive_12_mon',
+            'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
+            'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
+            'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
+            'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn',
+            'Income_Category_Churn', 'Card_Category_Churn']
+
+    X[cols] = encoded_df[cols]
     test_perform_feature_engineering(encoded_df, 'Churn')
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=42) 
-    test_train_models(X_train, X_test, y_train, y_test)
-    
+
+    X_training_set, X_testing_set, y_training_set, y_testing_set = train_test_split(
+        X, y, test_size=0.3, random_state=42)
+    test_train_models(X_training_set, X_testing_set,
+                      y_training_set, y_testing_set)
